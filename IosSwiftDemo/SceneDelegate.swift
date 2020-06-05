f2854b640a77961e97a4173b28681a9887b62142
席聪 <user.cong@gmail.com>
2020-06-05 17:30:20 +0800

    标注一个注意点

--- IosSwiftDemo/SceneDelegate.swift
+++ IosSwiftDemo/SceneDelegate.swift
@@ -21,8 +21,8 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
-        
+//        guard let _ = (scene as? UIWindowScene) else { return }
+        //⚠️注意，这里的UIWindow必须self.window = UIWindow(windowScene: windowScene)。不然出来是黑屏
         if let windowScene = scene as? UIWindowScene {
             self.window = UIWindow(windowScene: windowScene)
 //            let uINavigationController = UINavigationController(rootViewController: mainVc)
